ba1db214fc47301a2b2f603034495cccab053aff
Niklas Dusenlund <niklas@dusenlund.se>
2021-04-14 10:14:02 +0200

    Find toml files
    Automatically search for Cargo.toml toml file in case toml file isn't
    provided.

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
 # toml-echo
 
+This is a very simple tool for printing single values out of toml files.
+
+It can be used when packaging rust applications to get the version from the manifest file.
+
+If a tomfile doesn't contain any path segment separators (e.g. "Cargo.toml"), then the file will be
+searched after upwards from the current working dir. Otherwise, if the tomlfile argument has
+separators (e.g. "./Cargo.toml") it will only be searched for in the current working directory.
+
 
 ## Example usage
 
@@ -7,5 +15,5 @@
 ```sh
 #!/usr/bin/env bash
 
-VERSION=$(toml-echo Cargo.toml package.version)
+VERSION=$(toml-echo -f Cargo.toml package.version)
 ```
--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,83 @@
 use std::fs::File;
 use std::io::Read;
-use std::path::PathBuf;
+use std::ffi::OsStr;
+use std::path::{Path,PathBuf};
 use structopt::StructOpt;
 use toml::Value;
 
 #[derive(StructOpt, Debug)]
 #[structopt(name = "toml-echo")]
 struct Opt {
-    #[structopt(name = "FILE", parse(from_os_str))]
-    tomlfile: PathBuf,
+    #[structopt(long="tomlfile", short="f", parse(from_os_str))]
+    tomlfile: Option<PathBuf>,
     #[structopt(name = "QUERY")]
     query: String,
+}
+
+fn find_nearest_file<F: AsRef<OsStr>>(path: &Path, filename: F) -> Option<PathBuf> {
+    for p in path.ancestors() {
+        let mut candidate = p.to_path_buf();
+        candidate.push(filename.as_ref());
+        if candidate.is_file() {
+            return Some(candidate);
+        }
+    }
+    None
 }
 
 fn main() {
     let opt = Opt::from_args();
 
+    let tomlfile = match opt.tomlfile {
+        Some(file) => {
+            // Will only panic if file ends in ".."
+            let has_ancestors = file == file.file_name().unwrap();
+            if !has_ancestors {
+                file
+            } else if let Some(file) = find_nearest_file(&std::env::current_dir().unwrap(), file) {
+                file
+            } else {
+                eprintln!("No tomfile found");
+                return
+            }
+        },
+        None => {
+            eprintln!("No tomfile found");
+            return
+        },
+    };
+
     let mut file_content = String::new();
-    File::open(opt.tomlfile)
-        .unwrap()
-        .read_to_string(&mut file_content)
-        .unwrap();
+    match File::open(tomlfile) {
+        Ok(mut f) => {
+            if let Err(e) = f.read_to_string(&mut file_content) {
+                eprintln!("Couldn't read file: {}", e);
+                return;
+            }
+        },
+        Err(e) => {
+            eprintln!("Couldn't open file: {}", e);
+            return;
+        },
+    };
 
     let value = file_content.parse::<Value>().unwrap();
     let mut inner_value = None;
-    for path in opt.query.split(".") {
+    for path in opt.query.split('.') {
         if inner_value.is_none() {
             inner_value = value.get(path);
             continue;
         }
+        // Unwrap never panics because of check above
         inner_value = inner_value.unwrap().get(path);
     }
+    // Unwrap never panics because of check above
+    let inner_value = inner_value.unwrap();
 
-    // Default printer prints strings qouted which we don't want
-    if let Some(value) = inner_value.unwrap().as_str() {
+    // Default printer prints strings quoted which we don't want
+    if let Some(value) = inner_value.as_str() {
         println!("{}", value);
         return;
     }
-    println!("{}", inner_value.unwrap());
+    println!("{}", inner_value);
 }
